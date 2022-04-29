037416d8eb8396d7febaab4bb10164a8e51580c7
sovamorco <sovamorco@sovamor.co>
2022-04-29 13:35:27 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='SovamorcoCommon',
+    version='0.0.3',
+    description='Common utils',
+    author='Sovamorco',
+    author_email='sovamorco@sovamor.co',
+    url='https://github.com/Sovamorco/common',
+    packages=['common'],
+)
