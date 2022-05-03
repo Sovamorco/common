8f53a41191ad8b6e9c6c7031b6f93f2a1d4387f3
sovamorco <sovamorco@sovamor.co>
2022-05-03 19:18:45 +0300

    fix requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from distutils.core import setup
 
+with open('requirements.txt', 'r') as rf:
+    # .readlines() does not strip newlines
+    requirements = rf.read().splitlines()
+
 setup(
     name='SovamorcoCommon',
-    version='0.0.3',
+    version='0.0.4',
     description='Common utils',
     author='Sovamorco',
     author_email='sovamorco@sovamor.co',
     url='https://github.com/Sovamorco/common',
     packages=['common'],
+    install_requires=requirements,
 )
