421723e4d016bee470683ef68dcc251e1e644915
sovamorco <sovamorco@sovamor.co>
2023-01-31 02:46:59 +0400

    change setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-from distutils.core import setup
-from json import load
+from setuptools import setup
 
-with open('requirements.json', 'r') as rf:
-    reqs = load(rf)
-requirements = reqs.pop('')
-extras = reqs
-
-setup(
-    name='Common',
-    version='0.1.0',
-    description='Common utils',
-    author='Sovamorco',
-    author_email='sovamorco@sovamor.co',
-    url='https://github.com/Sovamorco/common',
-    packages=['common'],
-    install_requires=requirements,
-    extras_require=extras,
-)
+setup()
