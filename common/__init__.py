4d1fccc96744b448b8dba805d27077e52523163d
sovamorco <sovamorco@sovamor.co>
2024-09-16 21:47:50 +0400

    remove async vault client

--- common/__init__.py
+++ common/__init__.py
@@ -3,11 +3,6 @@ from .vault_client import VaultClient
 
 # ImportError raised inside these modules means that optional extras for it were not installed
 try:
-    from .vault_client_async import AsyncVaultClient
-except ImportError:
-    pass
-
-try:
     from .sql import SQLClient
 except ImportError:
     pass
